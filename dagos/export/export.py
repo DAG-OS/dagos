8537efc61b8fbd168ab709618b3777f8fce0d828
Lucas Resch <lucas.resch@gmx.de>
2022-01-08 19:32:26 +0100

    🔊 Improve logs and error handling

--- dagos/export/export.py
+++ dagos/export/export.py
@@ -16,33 +16,37 @@ class ExportError(Exception):
 def get_container_backend():
     container_backends = ["podman", "docker"]
     logging.debug(
-        f"Check for a supported container backend: {', '.join(container_backends)}"
+        f"Checking for a supported container backend: {', '.join(container_backends)}"
     )
     for backend in container_backends:
         if shutil.which(backend):
-            logging.info(f"Using {backend} as container backend")
+            logging.info(f"Detected {backend} as container backend")
             return backend
-    raise ExportError(
-        f"""
-        No container backend available!
-        Please install one of {', '.join(container_backends)} first.
-        """
+    logging.critical(
+        f"Please install a supported container backend ({', '.join(container_backends)})"
     )
+    exit(1)
 
 
 def start_container(backend, image):
     logging.debug("Starting container from provided image")
-    subprocess.run(
+    run_result = subprocess.run(
         f"{backend} run -t {image} sh -c 'ls / > /dev/null'",
         shell=True,
-        check=True,
+        stderr=subprocess.PIPE,
     )
+    if run_result.returncode != 0:
+        logging.critical(
+            f"Failed to start container for image '{image}':\n{run_result.stderr.decode('utf-8')}"
+        )
+        exit(1)
+
     logging.debug("Grabbing container ID")
     container_id = (
         subprocess.run(
             f"{backend} ps --last 1 | awk -F '[[:space:]]+' '$2 ~ /{image}/ {{ print $1 }}'",
             shell=True,
-            check=True,  # Maybe fail ourselves?
+            check=True,
             capture_output=True,
         )
         .stdout.decode("utf-8")
@@ -53,26 +57,28 @@ def start_container(backend, image):
 
 
 def get_image_name(backend, container_id):
+    container_exists = subprocess.run([backend, "container", "exists", container_id])
+    if container_exists.returncode != 0:
+        logging.critical(f"No container exists with provided ID '{container_id}'!")
+        exit(1)
     image = (
         subprocess.run(
             f"{backend} ps --all --filter 'id={container_id}' | awk -F '[[:space:]]+' '$1 ~ /{container_id}/ {{ print $2 }}'",
             shell=True,
-            check=True,  # Maybe fail ourselves?
+            check=True,
             capture_output=True,
         )
         .stdout.decode("utf-8")
         .strip()
     )
+    if len(image) == 0:
+        raise ExportError(f"Failed to get image name for container '{container_id}'!")
     logging.debug(f"Image for container '{container_id}' is '{image}'")
     return image
 
 
 @click.command(no_args_is_help=True)
-@optgroup.group(
-    "Container export selection",
-    cls=RequiredMutuallyExclusiveOptionGroup,
-    help="Provide one of the following for the export.",
-)
+@optgroup.group("Export selection", cls=RequiredMutuallyExclusiveOptionGroup)
 @optgroup.option("--container-id", help="A container ID.")
 @optgroup.option("--image", help="A fully qualified container image.")
 @click.option("--output", default="/tmp/dagos-export", help="Output directory on disk.")
@@ -87,18 +93,22 @@ def export(image, container_id, output):
     else:
         container_id = start_container(backend, image)
 
-    logging.info("Creating output directory")
+    logging.debug("Creating output directory")
     Path(output).mkdir(parents=True, exist_ok=True)
+    logging.info("Created output directory")
 
     image_name = re.sub("(:|/)", "_", image)
     archive = f"{output}/{image_name}.tar"
     logging.debug(f"Exporting '{container_id}' to '{archive}'")
     subprocess.run([backend, "export", "--output", archive, container_id], check=True)
 
-    # TODO:Package additional files together with exported tar file
+    # TODO: Package additional files together with exported tar file
 
     if image:
-        logging.info(f"Removing started container with ID '{container_id}")
-        subprocess.run([backend, "rm", container_id], check=True)
+        logging.debug(f"Removing container '{container_id}'")
+        subprocess.run(
+            [backend, "rm", container_id], check=True, stdout=subprocess.DEVNULL
+        )
+        logging.info(f"Removed container '{container_id}'")
 
     # TODO: Remove temporary files
--- dagos/main.py
+++ dagos/main.py
@@ -5,11 +5,18 @@ from dagos.export.export import export
 
 
 @click.group()
-def main():
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Enter verbose mode."
+)
+def main(verbose):
     logging.addLevelName(logging.WARNING, "WARN")
     logging.addLevelName(logging.CRITICAL, "FATAL")
     log_format = "{asctime} [{levelname:<5s}] {message}"
-    logging.basicConfig(level=logging.DEBUG, format=log_format, style="{")
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format=log_format,
+        style="{",
+    )
 
 
 main.add_command(export)
