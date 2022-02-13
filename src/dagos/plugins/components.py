4daa745c07e2e2d38c702c0d74e456e3f7881906
Lucas Resch <lucas.resch@gmx.de>
2022-02-13 09:46:12 +0100

    🚚 Move commands into a separate file

--- src/dagos/plugins/components.py
+++ src/dagos/plugins/components.py
@@ -1,55 +1,10 @@
 import typing as t
-from abc import ABC, abstractmethod
-from enum import Enum
+from abc import ABC
 
 import click
 import rich_click
 
-
-class CommandType(Enum):
-    """The supported commands for software components."""
-
-    MANAGE = "manage"
-    INSTALL = "install"
-    UNINSTALL = "uninstall"
-    UPDATE = "update"
-    CONFIGURE = "configure"
-    VERIFY = "verify"
-
-
-class Command(ABC):
-    """Base class for commands."""
-
-    def __init__(self, type: CommandType) -> None:
-        self.type = type
-
-    def build(self, name: t.Optional[str] = None) -> click.Command:
-        """Build a Click command for the CLI.
-
-        Note: The command doc string is used as the help text for the command.
-
-        Args:
-            name (str, optional): The name of the command. If none is provided,
-            the command type is used as the name. Defaults to None.
-
-        Returns:
-            click.Command: A Click command to be used in a CLI.
-        """
-        help_text = self.__doc__
-        name = self.type.value if name == None else name
-        return click.Command(name=name, help=help_text, callback=self.execute)
-
-    @abstractmethod
-    def execute(self) -> None:
-        """Execute the command."""
-        raise NotImplementedError()
-
-
-class InstallCommand(Command):
-    """Base class for commands with the INSTALL type."""
-
-    def __init__(self) -> None:
-        super().__init__(CommandType.INSTALL)
+from .commands import Command, CommandType, InstallCommand
 
 
 class SoftwareComponent(ABC):
